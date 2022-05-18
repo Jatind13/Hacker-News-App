178aa0999358a23d3873fd37ad2dc91cb39467ba
Jatin Dixit <jatindixit1305@gmail.com>
2022-05-18 13:35:44 +0530

    basic UI created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>{}</div>
-        {/* this is how we use javascrit in reactjs */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* first let's setup the UI */}
+      <section className="section">
+        <form>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search the news"
+          />
+          <button>Search</button>
+        </form>
+        <article>
+          <h1>First posts tiltle here</h1>
+          {/* here href will get the link from API call */}
+          <a href="">Read full story</a>
+        </article>
+
+        {/* building how each card will look like */}
+        <article>
+          <div>
+            <h2>Heading 2</h2>
+            <ul>
+              <li>By Jatin Dixit</li>
+              <li>
+                <a href="">Read full story</a>
+              </li>
+            </ul>
+            <p>Date(point)</p>
+          </div>
+        </article>
+      </section>
+    </>
   );
 }
 
